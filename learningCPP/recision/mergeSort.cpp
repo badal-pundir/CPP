65bbf7f5fdb668e9e34e06a3460b70536106ad6d
CLOUDY <badalpundir2@gmail.com>
2024-09-20 04:01:00 +0530

    merge sort

---
+++ learningCPP/recision/mergeSort.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+void mergeSort(int arr[], int left, int right {
+    if()
+    int middle = (left + right)/2;
+    mergeSort(arr, left, middle - 1);
+    mergeSort(arr, middle, right);
+})
+int main() {
+    vector<int> arr = {3, 1, 2, 4, 1, 5, 2, 6, 4};
+    int arr[] = {3, 1, 2, 4, 1, 5, 2, 6, 4};
+    int n = 9;
+
+    mergeSort(arr, 0, n - 1);
+    return 0;
+}
