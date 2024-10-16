85c90b687de1ada4f373ada5c52b9f22a641393a
CLOUDY <badalpundir2@gmail.com>
2024-10-16 17:45:54 +0530

    add ans  vecot<vector<int>>

--- learningCPP/recision/printAllCombination.cpp
+++ learningCPP/recision/printAllCombination.cpp
@@ -1,12 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void printAllComb(vector<int> arr, int st,  int r, vector<int> newlist) {
+void printAllComb(vector<int> arr, int st,  int r, vector<int> newlist, vector< vector<int>> ans) {
     // base condition
     if(r == 0){
-        cout<<"[";
-        for(auto &it: newlist) cout<< it << " ";
-        cout<<"]";
+        // cout<<"[";
+        // for(auto &it: newlist) cout<< it << " ";
+        // cout<<"]";
+        ans.push_back(newlist);
+        for(auto & it: newlist) {
+            cout<< it;
+        }
         cout<<endl;
         // newlist.pop_back();
 
@@ -15,7 +19,7 @@ void printAllComb(vector<int> arr, int st,  int r, vector<int> newlist) {
     // main coding
     for(int i = st; i < static_cast<int>(arr.size()); i++) {
         newlist.push_back(arr[i]);
-        printAllComb(arr,i + 1,  r - 1, newlist);
+        printAllComb(arr,i + 1,  r - 1, newlist, ans);
     	newlist.pop_back();
     }
 }
@@ -23,7 +27,9 @@ int main() {
     vector<int> arr = {1, 2, 3};
     int r = 2;
     // int newsize = 2 * 3;
+    vector<vector<int>> ans;
     vector<int> newlist;
-    printAllComb(arr,0, r, newlist);
+    printAllComb(arr,0, r, newlist, ans);
+    
     return 0;
 }
